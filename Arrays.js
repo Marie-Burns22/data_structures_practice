f954fb3c8ac98cc958805a9401d9ed1ffa441b89
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2020-01-06 09:22:30 -0800

    Add practice file

---
+++ Arrays.js
@@ -0,0 +1,40 @@
+// variables for testing
+let testStringA = 'cbcddc'
+let testStringB = 'ddccb1'
+
+
+// Checks if all characters in a string are unique
+function isUnique(string) {
+    if (string.length > 128) return false; // if using ASCII which only has 128 characters
+    let checked = {};
+    for (let i = 0; i < string.length; i ++) {
+        let val = string[i];
+        if(checked[val]) return false;
+        checked[val] = true;
+    };
+    return true;
+};
+
+// console.log(isUnique(testStringA));
+// console.log(isUnique(testStringB));
+
+function isPermutation(strA, strB) {
+    if(strA.length !== strB.length) return false;
+    let aChars = {};
+    let bChars = {};
+    for (let i = 0; i < strA.length; i ++) {
+        let val = strA[i];
+        (aChars[val]) ? aChars[val] ++ : aChars[val] = 1;
+        console.log(val, aChars[val]);
+    }
+    for (let i = 0; i < strB.length; i ++) {
+        let val = strB[i];
+        (bChars[val]) ? bChars[val] ++ : bChars[val] = 1;
+    }
+    for (const key in aChars) {
+        if (aChars[key] !== bChars[key]) return false;
+    }
+    return true;
+}
+
+// console.log(isPermutation(testStringB, testStringA))
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# data_structures_practice
+## Purpose
+These files are to practice algorithms for different structures.
+
+## How to run
+In VS Code open the file and uncomment the appropriate lines to run a specific function. Add any additional variables to run more tests. Use the Debug Console (in the same pane as the integrated TERMINAL) and select the debug sidebar from the left of the screen. From here you can run the code. Note: to see the return values in the debug console, you must wrap your function calls in console.log().
